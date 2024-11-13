fc04846761b8367e0e2ad8aa17bea830426fb174
st17931 <st17931@gmail.com>
2024-11-13 10:47:59 +0530

    Now user filled from will be shown to us

--- script.js
+++ script.js
@@ -4,18 +4,41 @@ document.addEventListener("DOMContentLoaded", () => {
     if (contactForm) {
         contactForm.addEventListener("submit", (event) => {
             event.preventDefault();
-            const name = document.getElementById("name").value.trim();
-            const email = document.getElementById("email").value.trim();
-            const subject = document.getElementById("subject").value.trim();
-            const message = document.getElementById("message").value.trim();
+            const name = document.getElementById("name");
+            const email = document.getElementById("email");
+            const subject = document.getElementById("subject");
+            const message = document.getElementById("message");
+            const contact = document.getElementById("number");
 
-            if (name === "" || email === "" || subject === "" || message === "") {
+            if (name.value.trim() === "" || email.value.trim() === "" || subject.value.trim() === "" || message.value.trim() === "" || contact.value.trim() === "") {
                 alert("Please fill out all fields.");
-            } else if (!validateEmail(email)) {
-                alert("Please enter a valid email address.");
             } else {
-                alert("Message sent successfully!");
-                contactForm.reset();
+
+
+
+                const serviceID = "service_uf0h6k4"; // Replace with your service ID
+                const templateID = "template_jpbourm"; // Replace with your template ID
+
+                emailjs.send(serviceID, templateID, {
+                    to_name: 'Softnet Solutions',
+                    from_name: name.value.trim(),
+                    message: `Subject:-${subject.value.trim()} Message:-${message.value.trim()} Phone No:-${contact.value.trim()} Email ID:-${email.value.trim()}`,
+                    //to_email: 'recipient@example.com', // Add more parameters if needed
+                })
+                    .then(response => {
+                        console.log('Email sent successfully!', response.status, response.text);
+                        name.value = "";
+                        email.value = "";
+                        subject.value = "";
+                        message.value = "";
+                        contact.value = "";
+                    })
+                    .catch(error => {
+                        console.error('Error sending email:', error);
+                    });
+
+
+
             }
         });
     }
